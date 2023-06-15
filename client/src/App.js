7faaec14e47d2b265a8677d2c2779adc3eb083a8
F1uke 🐻 <f1uke@Flxke-macbook.local>
2023-06-16 03:48:02 +0700

    RM google map react ver1

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect, Component} from "react";
 import axios from "axios";
 import Navbar from "./component/navbar";
 import Login from "./Pages/login";
-import {GoogleApiWrapper, Map} from "google-maps-react";
+
 
 function App() {
   return (
